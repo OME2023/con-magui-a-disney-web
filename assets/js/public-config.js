0e72bb39d45ba842383cdaaac815960d3d6093fd
OME2023 <126077066+OME2023@users.noreply.github.com>
2026-08-01 13:27:36 -0300

    fix: use cmd promotions endpoint

--- assets/js/public-config.js
+++ assets/js/public-config.js
@@ -1,2 +1,2 @@
 window.PUBLIC_INSURANCE_BANNER_URL = "https://pub-1703e0d90f9d43a3999f7db9f0df8a0d.r2.dev/agencies/2-cmd/insurance/banner/current/assistcard-auto.jpg";
-window.PUBLIC_PROMOTIONS_API_URL = "https://tass.itintegraltech.com/api/public/promotions/con-magui-a-disney";
+window.PUBLIC_PROMOTIONS_API_URL = "https://tass.itintegraltech.com/api/public/promotions/cmd";
